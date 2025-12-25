f73d82f31126d678bc05fdda5bff57be2c86d7c0
marco vitturini <166159056+MVittu@users.noreply.github.com>
2025-12-25 20:58:48 +0100

    Add school starting skills configuration

---
+++ school_starting_skills.js
@@ -0,0 +1,340 @@
+// school_starting_skills.js
+// Mappa: Nome Scuola -> { pick: numero, options: [abilità] }
+// Nomi abilità normalizzati per combaciare con skills.js (es. "Arti Marziali [Distanza]").
+
+window.SCHOOL_STARTING_SKILLS = {
+  // ===== GRANCHIO =====
+  "Scuola del Difensore Hida": {
+    pick: 5,
+    options: [
+      "Arti Marziali [Distanza]",
+      "Arti Marziali [Mischia]",
+      "Arti Marziali [Senza Armi]",
+      "Allenamento",
+      "Meditazione",
+      "Sopravvivenza",
+      "Tattica",
+    ],
+  },
+  "Scuola del Ricognitore Hiruma": {
+    pick: 5,
+    options: [
+      "Arti Marziali [Distanza]",
+      "Arti Marziali [Mischia]",
+      "Allenamento",
+      "Criminalità",
+      "Sopravvivenza",
+      "Tattica",
+      "Teologia",
+    ],
+  },
+  "Scuola dell’Ingegnere Kaiu": {
+    pick: 5,
+    options: [
+      "Arti Marziali [Distanza]",
+      "Comando",
+      "Forgia",
+      "Lavori Pesanti",
+      "Medicina",
+      "Moda",
+      "Teologia",
+    ],
+  },
+  "Scuola del Purificatore Kuni": {
+    pick: 3,
+    options: [
+      "Arti Marziali [Mischia]",
+      "Criminalità",
+      "Medicina",
+      "Sensibilità",
+      "Sopravvivenza",
+      "Teologia",
+    ],
+  },
+  "Scuola del Mercante Yasuki": {
+    pick: 5,
+    options: [
+      "Arti Marziali [Distanza]",
+      "Commercio",
+      "Composizione",
+      "Cortesia",
+      "Cultura",
+      "Governo",
+      "Moda",
+    ],
+  },
+
+  // ===== GRU =====
+  "Scuola dell’Artefice Asahina": {
+    pick: 3,
+    options: ["Cultura", "Cortesia", "Estetica", "Giochi", "Moda", "Teologia"],
+  },
+  "Scuola del Guerriero di Ferro Daidoji": {
+    pick: 5,
+    options: [
+      "Arti Marziali [Mischia]",
+      "Allenamento",
+      "Comando",
+      "Cortesia",
+      "Cultura",
+      "Governo",
+      "Tattica",
+    ],
+  },
+  "Scuola del Diplomatico Doji": {
+    pick: 5,
+    options: [
+      "Arti Marziali [Distanza]",
+      "Composizione",
+      "Cultura",
+      "Cortesia",
+      "Estetica",
+      "Governo",
+      "Moda",
+    ],
+  },
+  "Scuola del Duellista Kakita": {
+    pick: 5,
+    options: [
+      "Arti Marziali [Mischia]",
+      "Allenamento",
+      "Cortesia",
+      "Forgia",
+      "Meditazione",
+      "Moda",
+      "Sensibilità",
+    ],
+  },
+
+  // ===== DRAGO =====
+  "Scuola del Mistico Agasha": {
+    pick: 3,
+    options: [
+      "Arti Marziali [Senza Armi]",
+      "Allenamento",
+      "Forgia",
+      "Medicina",
+      "Meditazione",
+      "Teologia",
+    ],
+  },
+  "Scuola dell’Investigatore Kitsuki": {
+    pick: 5,
+    options: [
+      "Arti Marziali [Mischia]",
+      "Criminalità",
+      "Cultura",
+      "Governo",
+      "Medicina",
+      "Sensibilità",
+      "Sopravvivenza",
+    ],
+  },
+  "Scuola dell’Adepto dei Due-Cieli Mirumoto": {
+    pick: 5,
+    options: [
+      "Arti Marziali [Mischia]",
+      "Allenamento",
+      "Comando",
+      "Composizione",
+      "Meditazione",
+      "Tattica",
+      "Teologia",
+    ],
+  },
+
+  // ===== LEONE =====
+  "Scuola del Comandante Akodo": {
+    pick: 5,
+    options: [
+      "Arti Marziali [Distanza]",
+      "Arti Marziali [Mischia]",
+      "Allenamento",
+      "Comando",
+      "Governo",
+      "Meditazione",
+      "Tattica",
+    ],
+  },
+  "Scuola del Bardo Ikoma": {
+    pick: 5,
+    options: [
+      "Arti Marziali [Distanza]",
+      "Composizione",
+      "Cultura",
+      "Cortesia",
+      "Esibizione",
+      "Sensibilità",
+      "Tattica",
+    ],
+  },
+  "Scuola del Medium Kitsu": {
+    pick: 3,
+    options: [
+      "Cortesia",
+      "Governo",
+      "Meditazione",
+      "Esibizione",
+      "Sopravvivenza",
+      "Teologia",
+    ],
+  },
+  "Scuola del Furioso Matsu": {
+    pick: 5,
+    options: [
+      "Arti Marziali [Distanza]",
+      "Arti Marziali [Mischia]",
+      "Arti Marziali [Senza Armi]",
+      "Allenamento",
+      "Comando",
+      "Lavori Pesanti",
+      "Sopravvivenza",
+    ],
+  },
+
+  // ===== FENICE =====
+  "Scuola del Maestro del Sapere Asako": {
+    pick: 5,
+    options: [
+      "Arti Marziali [Senza Armi]",
+      "Cultura",
+      "Governo",
+      "Medicina",
+      "Esibizione",
+      "Sensibilità",
+      "Teologia",
+    ],
+  },
+  "Scuola dell’Elementalista Isawa": {
+    pick: 3,
+    options: [
+      "Composizione",
+      "Cortesia",
+      "Medicina",
+      "Meditazione",
+      "Esibizione",
+      "Teologia",
+    ],
+  },
+  "Scuola del Guardiano Shiba": {
+    pick: 5,
+    options: [
+      "Arti Marziali [Mischia]",
+      "Allenamento",
+      "Cortesia",
+      "Meditazione",
+      "Sopravvivenza",
+      "Tattica",
+      "Teologia",
+    ],
+  },
+
+  // ===== SCORPIONE =====
+  "Scuola del Manipolatore Bayushi": {
+    pick: 5,
+    options: [
+      "Arti Marziali [Senza Armi]",
+      "Comando",
+      "Cortesia",
+      "Criminalità",
+      "Moda",
+      "Esibizione",
+      "Sensibilità",
+    ],
+  },
+  "Scuola dell’Infiltratore Shosuro": {
+    pick: 5,
+    options: [
+      "Arti Marziali [Mischia]",
+      "Arti Marziali [Senza Armi]",
+      "Allenamento",
+      "Cortesia",
+      "Giochi",
+      "Criminalità",
+      "Esibizione",
+    ],
+  },
+  "Scuola dell’Illusionista Soshi": {
+    pick: 3,
+    options: [
+      "Allenamento",
+      "Cortesia",
+      "Giochi",
+      "Criminalità",
+      "Esibizione",
+      "Teologia",
+    ],
+  },
+  "Scuola del Maestro dei Sigilli Yogo": {
+    pick: 3,
+    options: [
+      "Arti Marziali [Mischia]",
+      "Arti Marziali [Senza Armi]",
+      "Allenamento",
+      "Composizione",
+      "Meditazione",
+      "Teologia",
+    ],
+  },
+
+  // ===== UNICORNO =====
+  "Scuola del Commerciante Ide": {
+    pick: 5,
+    options: [
+      "Arti Marziali [Distanza]",
+      "Commercio",
+      "Cortesia",
+      "Giochi",
+      "Lavori Pesanti",
+      "Medicina",
+      "Sopravvivenza",
+    ],
+  },
+  "Scuola del Maestro del Meishōdō Iuchi": {
+    pick: 3,
+    options: [
+      "Arti Marziali [Mischia]",
+      "Estetica",
+      "Meditazione",
+      "Moda",
+      "Sopravvivenza",
+      "Teologia",
+    ],
+  },
+  "Scuola del Conquistatore Moto": {
+    pick: 5,
+    options: [
+      "Arti Marziali [Mischia]",
+      "Arti Marziali [Senza Armi]",
+      "Allenamento",
+      "Comando",
+      "Esibizione",
+      "Sopravvivenza",
+      "Tattica",
+    ],
+  },
+  "Scuola del Pioniere Shinjo": {
+    pick: 5,
+    options: [
+      "Arti Marziali [Distanza]",
+      "Arti Marziali [Mischia]",
+      "Commercio",
+      "Criminalità",
+      "Medicina",
+      "Sopravvivenza",
+      "Tattica",
+    ],
+  },
+  "Scuola della Vergine Guerriera Utaku": {
+    pick: 5,
+    options: [
+      "Arti Marziali [Mischia]",
+      "Allenamento",
+      "Comando",
+      "Forgia",
+      "Governo",
+      "Meditazione",
+      "Sopravvivenza",
+    ],
+  },
+};
